3a335753b549a81cac737a54deda15167a2811a1
aramisf <aramis.fernandes@gmail.com>
2015-09-13 11:58:56 -0300

    Adds a sample application example

---
+++ pubsup/app.js
@@ -0,0 +1,34 @@
+// js pragmas ;)
+'use strict';
+
+// Loading an express instance inside var 'app'
+var express = require('express');
+var app = express();
+
+
+// Middleware: the place where all requests pass through before going to the
+// routes.
+//
+// This one expects post requests to have content in JSON format. If there is
+// JSON data, then it will be parsed by the .json() method of the express
+// module.
+app.use(express.json());
+// --- /Middleware
+
+
+// Routes:
+//
+// Defining a route to '/'. The anonymous function passed to post() method on app
+// (an express instance), receives two parameters, the request and the response.
+app.post('/', function(req, res) {
+
+  // The send() method on the res(ponse) parameter obviously sends a response to
+  // the client, but this comment is here because this is app is a tutorial.
+  res.send('hello world');
+});
+// --- /Routes
+
+
+// Set a listening port to our app:
+app.listen(2015);
+
